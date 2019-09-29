918eaf128168286a6232392c0cacc672933c6a3c
Valeriy Kovetskiy <kovetskiy.valeriy@gmail.com>
2019-09-29 18:26:02 +0300

    Added Cookie Authentication

---
+++ ShopAdoAspNet/ShopAdoAspNet/Controllers/AuthController.cs
@@ -0,0 +1,46 @@
+﻿using ShopAdoAspNet.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Owin;
+using Microsoft.Owin;
+using Microsoft.Owin.Security;
+using System.Security.Claims;
+
+namespace ShopAdoAspNet.Controllers
+{
+    public class AuthController : Controller
+    {
+        public ActionResult Login()
+        {
+            return View(new LoginViewModel());
+        }
+
+        [HttpPost]
+        public ActionResult Login(LoginViewModel model)
+        {
+            if (model.Username.Equals("admin") && model.Password.Equals("admin"))
+            {
+                var identity = new ClaimsIdentity(new[] 
+                {
+                    new Claim(ClaimTypes.Name, model.Username),
+                    new Claim(ClaimTypes.Email, model.Username)
+                }, "CookieAuth");
+
+                var owinContext = Request.GetOwinContext();
+                owinContext.Authentication.SignIn(identity);
+            }
+
+            return Redirect(Request.UrlReferrer.ToString());
+        }
+
+        public ActionResult Logout()
+        {
+            var owinContext = Request.GetOwinContext();
+            owinContext.Authentication.SignOut("CookieAuth");
+            return Redirect(Request.UrlReferrer.ToString());
+        }
+    }
+}
---
+++ ShopAdoAspNet/ShopAdoAspNet/Startup.cs
@@ -0,0 +1,20 @@
+﻿using Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ShopAdoAspNet
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder builder)
+        {
+            builder.UseCookieAuthentication(new Microsoft.Owin.Security.Cookies.CookieAuthenticationOptions
+            {
+                AuthenticationType = "CookieAuth",
+                LoginPath = new Microsoft.Owin.PathString("/Auth/Login/")
+            });
+        }
+    }
+}
