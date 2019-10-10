14fa5bb0aa40e2f43db4bfdc1ee1f9a70c4cf921
Valerii Kovetskyi <valerii.k@sbtech.com>
2019-10-10 17:57:05 +0300

    Added base functionality for cart

---
+++ ShopAdoAspNet/ShopAdoAspNet/Controllers/CartController.cs
@@ -0,0 +1,34 @@
+﻿using ShopAdo.BLL.DTO;
+using ShopAdo.BLL.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ShopAdoAspNet.Controllers
+{
+    [Authorize]
+    public class CartController : Controller
+    {
+        private readonly GoodService _goodService;
+        private IList<GoodDTO> _cart;
+        
+        public CartController(GoodService goodService)
+        {
+            _goodService = goodService;
+        }
+
+        public void AddToCart(int goodId)
+        {
+            _cart = (Session["Cart"] as List<GoodDTO>) ?? new List<GoodDTO>();
+            _cart.Add(_goodService.Get(goodId));
+            Session["Cart"] = _cart;
+        }
+
+        public List<GoodDTO> GetCart()
+        {
+            return (Session["Cart"] as List<GoodDTO>) ?? new List<GoodDTO>();
+        }
+    }
+}
